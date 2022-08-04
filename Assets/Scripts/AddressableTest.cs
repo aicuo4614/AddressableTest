2fb6b95e2b782ff272fd208c28af572a5e382599
hellojimmywu <505861829@qq.com>
2022-08-04 14:49:23 +0800

    fixed bugs

--- Assets/Scripts/AddressableTest.cs
+++ Assets/Scripts/AddressableTest.cs
@@ -6,17 +6,33 @@ using UnityEngine.AddressableAssets;
 public class AddressableTest : MonoBehaviour
 {
     public string Name;
-
+    private List<GameObject> lst = new List<GameObject>();
     // Start is called before the first frame update
     async void Start()
     {
-        var res = await Addressables.LoadAssetAsync<GameObject>(Name).Task;
-        Instantiate(res);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
     }
+
+    private async void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            var res = await Addressables.LoadAssetAsync<GameObject>(Name).Task;
+            lst.Add(res);
+            Instantiate(res);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            while (lst.Count > 0)
+            {
+                var r = lst[0];
+                lst.RemoveAt(0);
+                Addressables.ReleaseInstance(r);
+                break;
+            }
+         
+        }   
+    }
+
+    
 }
